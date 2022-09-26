2df5e4f1b557366cde09f4dc596fade08cfee11e
Felipe Vasconcelos <felipevs@ciandt.com>
2022-09-26 15:31:46 -0300

    testes iniciais

--- src/test/java/com/felipe/arqsoftware/demo/BancarioApplicationTests.java
+++ src/test/java/com/felipe/arqsoftware/demo/BancarioApplicationTests.java
@@ -1,13 +1,45 @@
 package com.felipe.arqsoftware.demo;
 
+import com.felipe.arqsoftware.demo.model.Cliente;
+import com.felipe.arqsoftware.demo.model.ContaCorrente;
+import com.felipe.arqsoftware.demo.repository.ClienteRepository;
+import com.felipe.arqsoftware.demo.repository.ContaCorrenteRepository;
+import com.felipe.arqsoftware.demo.repository.ExtratoRepository;
+import com.felipe.arqsoftware.demo.service.ClienteService;
+import com.felipe.arqsoftware.demo.service.ContaCorrenteService;
+import com.felipe.arqsoftware.demo.service.ExtratoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BancarioApplicationTests {
 
+	@Autowired
+	ClienteService clienteService;
+	@Autowired
+	ContaCorrenteService contaCorrenteService;
+
+	@Autowired
+	ClienteRepository clienteRepository;
+	@Autowired
+	ContaCorrenteRepository contaCorrenteRepository;
+
 	@Test
-	void contextLoads() {
+	void finByIdShouldReturnOneClienteWhenCreateClientIsCalled() {
+		clienteService.createCliente(new Cliente(1L, "Nome", "CPF", null));
+		Assertions.assertEquals(1, clienteRepository.count());
 	}
+
+	@Test
+	void findByIdShouldReturnOneAccountWhenCreateAccountIsCalled() {
+		Cliente cliente = new Cliente(1L, "Nome", "CPF", null);
+		clienteService.createCliente(cliente);
+		ContaCorrente conta = new ContaCorrente(1L, 1, 123, cliente, 0.0);
+		contaCorrenteService.createAccount(conta);
+		Assertions.assertEquals(1, contaCorrenteRepository.count());
+	}
+
 
 }
