80281c3b8ca8eed4fb7c29ca2ccb8918532c3072
Felipe Vasconcelos <felipevs@ciandt.com>
2022-09-26 15:35:40 -0300

    teste de repository

--- src/test/java/com/felipe/arqsoftware/demo/BancarioApplicationTests.java
+++ src/test/java/com/felipe/arqsoftware/demo/BancarioApplicationTests.java
@@ -4,10 +4,8 @@ import com.felipe.arqsoftware.demo.model.Cliente;
 import com.felipe.arqsoftware.demo.model.ContaCorrente;
 import com.felipe.arqsoftware.demo.repository.ClienteRepository;
 import com.felipe.arqsoftware.demo.repository.ContaCorrenteRepository;
-import com.felipe.arqsoftware.demo.repository.ExtratoRepository;
 import com.felipe.arqsoftware.demo.service.ClienteService;
 import com.felipe.arqsoftware.demo.service.ContaCorrenteService;
-import com.felipe.arqsoftware.demo.service.ExtratoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +31,12 @@ class BancarioApplicationTests {
 	}
 
 	@Test
-	void findByIdShouldReturnOneAccountWhenCreateAccountIsCalled() {
+	void repositoryCountShouldReturnZeroWhenThereIsNoClientSaved() {
+		Assertions.assertEquals(0, clienteRepository.count());
+	}
+
+	@Test
+	void repositoryCountShouldReturnOneAccountWhenCreateAccountIsCalled() {
 		Cliente cliente = new Cliente(1L, "Nome", "CPF", null);
 		clienteService.createCliente(cliente);
 		ContaCorrente conta = new ContaCorrente(1L, 1, 123, cliente, 0.0);
